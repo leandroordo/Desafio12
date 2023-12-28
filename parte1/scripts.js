27f6e4098d4212ad947539b8f139ce362a43ee83
Leandro Ordoñez <leandro.o.ar@hotmail.com>
2023-12-28 01:01:06 -0300

    First commit

--- parte1/scripts.js
+++ parte1/scripts.js
@@ -1,10 +1,19 @@
 const smallestCircleWidth = 20; //Tamaño del círculo más pequeño, en porcentaje del contenedor
 const theme = "Minions";
-const colorDefinition = [
-  { name: "Minion yellow", value: "#fce03fff" },
-  { name: "Minion blue", value: "#0b5d85ff" },
-  { name: "Minion despicable me", value: "#ed2025ff" },
-  { name: "Minion Gru gray", value: "#93abadff" },
+
+class colorDefinition {
+  constructor(name, value) {
+    this.name = name;
+    this.value = value;
+  }
+}
+
+const colorDefinitionArray = [
+  new colorDefinition("Minion yellow", "#fce03fff"),
+  new colorDefinition("Minion blue", "#0b5d85ff"),
+  new colorDefinition("Minion despicable me", "#ed2025ff"),
+  new colorDefinition("Minion Gru gray", "#93abadff"),
+  new colorDefinition("Red", "red"),
 ];
 
 function appendTheme(themeName, where) {
@@ -14,10 +23,10 @@ function appendTheme(themeName, where) {
   where.appendChild(newSpan);
 }
 
-function loadColors(colorDefinition, select) {
+function loadColors(colors, select) {
   clearSelect(select);
 
-  for (const color of colorDefinition) {
+  for (const color of colors) {
     const newOption = new Option(color.name, color.value);
     select.add(newOption);
   }
@@ -63,15 +72,38 @@ function createCircles(howMany, where, clickEventFunction) {
   }
 }
 
-const title = document.getElementsByTagName("h1")[0];
-const select = document.getElementsByTagName("select")[0];
-
-appendTheme(theme, title);
-loadColors(colorDefinition, select);
-
-let whereDiv = document.getElementsByClassName("circles-section")[0];
-createCircles(4, whereDiv, clickHandler);
-
 function clickHandler() {
+  //Al hacer click sobre uno de ellos, cambie su color de fondo por el que esté seleccionado en el momento en el select.
+  const checkModoSuperpuesto = document.getElementsByTagName("input")[0];
+  if (checkModoSuperpuesto.checked) {
+    //Si el modo superpuesto está activado, se deberán pintar también los círculos que se encuentren detrás
+    let circle = this.nextSibling;
+    while (circle) {
+      circle.style.backgroundColor = select.value;
+      circle = circle.nextSibling;
+    }
+  }
+  //En caso contrario, solo cambiará el color del círculo clickeado
+  //(En ambos casos se cambia el color del círculo clickeado)
   this.style.backgroundColor = select.value;
 }
+
+const title = document.getElementsByTagName("h1")[0];
+const select = document.getElementsByTagName("select")[0];
+const circlesDiv = document.getElementsByClassName("circles-section")[0];
+const resetButton = document.getElementsByTagName("button")[0];
+
+appendTheme(theme, title);
+loadColors(colorDefinitionArray, select);
+
+//Crear en la vista 4 círculos concéntricos sin color de fondo, con borde negro
+createCircles(4, circlesDiv, clickHandler);
+
+resetButton.addEventListener("click", () => {
+  const circles = circlesDiv.getElementsByTagName("div");
+  const circlesArray = Array.prototype.slice.call(circles);
+  circlesArray.forEach((element, index) => {
+    //Borrar el color de todos los círculos
+    element.style.backgroundColor = "transparent";
+  });
+});
